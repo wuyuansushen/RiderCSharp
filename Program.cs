4053481e688333632f34c67601b9f7b46d71e628
FEG <wuyuansushen@gmail.com>
2020-10-25 00:28:27 +0800

    Rider C# start

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using static System.Console;
+
+namespace RiderCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WriteLine("Hello World!");
+            WriteLine("Rider First");
+        }
+    }
+}
